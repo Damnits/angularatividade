3c45790fd7e579a14c7fdc5df926324cdc0998c7
Pablo <36178185+Damnits@users.noreply.github.com>
2021-03-14 23:19:33 -0300

    intial-commit2

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('atividadeangular app is running!');
+    expect(await page.getTitleText()).toEqual('banco1 app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'atividadeangular'`, () => {
+  it(`should have as title 'banco1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('atividadeangular');
+    expect(app.title).toEqual('banco1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('atividadeangular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('banco1 app is running!');
   });
 });
